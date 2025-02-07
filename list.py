dca7d847625a1f4e9d7be482633c0dc6038f0710
Madlen N. Nedyalkova <madlennedyalkova@gmail.com>
2025-02-07 22:06:54 +0200

    Create list.py

---
+++ list.py
@@ -0,0 +1,14 @@
+list = ["blue","red", "green"]
+#square brackets
+
+#used to store multiple items in a single variable.
+
+#list items are ordered, changeable, and allow duplicate values
+
+print(len(list))
+
+#a list can contain different data types.
+
+#it is possible to use the list() constructor when creating a new list
+thislist = list(("apple", "banana", "cherry"))
+print(thislist)
