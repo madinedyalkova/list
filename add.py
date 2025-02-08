e1909ada29f4cd9f47c822573eb00cba252a8b9e
Madlen N. Nedyalkova <madlennedyalkova@gmail.com>
2025-02-08 15:01:42 +0200

    add.py

---
+++ add.py
@@ -0,0 +1,17 @@
+#append()
+
+list = ["car", "fruit", "vegetable"]
+list.append("toys")
+print(list)
+
+#insert
+
+list = ["car", "fruit", "vegetable"]
+list.insert(2, "toys")
+print(list)
+
+#extend
+list = ["car", "fruit", "vegetable"]
+list2 = ["a","b","c"]
+list.extend(list2)
+print(list)
